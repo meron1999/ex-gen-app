d84ab265a1ea8f02b9fe8c77a89dfb2750953c4f
OTAKE_PC\zxyv_ <zxyv_fujiww@yahoo.co.jp>
2025-06-13 09:40:39 +0900

    グーグルから取得したニュース一覧を表示

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +11,12 @@ var helloRouter = require('./routes/hello');
 
 var app = express();
 
+var session_opt={
+  secret:'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie:{maxAge: 60 * 60 * 1000} // 1 hour
+};
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -19,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(session_opt));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -33,7 +41,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};                                                                                                                                                                                   
 
   // render the error page
   res.status(err.status || 500);
--- routes/hello.js
+++ routes/hello.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const https = require('https');
+const parseString = require('xml2js').parseString;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('Hello', { 
-    title: 'Hello' ,
-    content:'これはサンプルのコンテンツです。<br>this is a sample content.'
+  var opt ={
+    host:'news.google.com',
+    // port: 443, // HTTPSのデフォルトポートなので、通常は不要
+    path:'/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja',
+  };
+  https.get(opt, (res2) => {
+    var body = '';
+    res2.on('data', (data) => {
+      body += data;
     });
-});
+    res2.on('end', () => {
+      parseString(body.trim(), (err, result) => {
+        console.log(result);
+        var data={
+          title: 'Google News',
+          content: result.rss.channel[0].item
+        };
+        res.render('Hello', data)
+        });
+      });
+    });
+  });
+
 
 module.exports = router;
